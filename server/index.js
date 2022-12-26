b36f3ae3320a11c293dbabd703f949da17dd8323
Abhishek Sharma <ohsharmaabhi03@gmail.com>
2022-12-26 20:31:25 +0530

    fix:react route issue on deploy

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use('/api/batch', batchRoutes);
 
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res)=>{
+app.get('/*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
